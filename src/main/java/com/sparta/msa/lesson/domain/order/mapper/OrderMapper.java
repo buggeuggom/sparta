6c7d3bec387e77e7294e6ac982b2252aa8724e2f
InKyuHwang001 <hwanginkyu42@naver.com>
2026-04-06 21:52:13 +0900

    Week2 Day3: Transaction(ACID), Persistence Consistency, Dirty Checking

--- src/main/java/com/sparta/msa/lesson/domain/order/mapper/OrderMapper.java
+++ src/main/java/com/sparta/msa/lesson/domain/order/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.sparta.msa.lesson.domain.order.mapper;
 
-import com.sparta.msa.lesson.domain.order.dto.request.OrderResponse;
+import com.sparta.msa.lesson.domain.order.dto.request.OrderRequest;
+import com.sparta.msa.lesson.domain.order.dto.response.OrderResponse;
 import com.sparta.msa.lesson.domain.order.entity.Order;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -9,8 +10,9 @@ import org.mapstruct.Mapping;
 public interface OrderMapper {
 
     // 여러 개의 @Mapping을 동시에 사용할 수 있음
-    @Mapping(target = "orderId", source = "id") // 이름이 다른 필드 매핑
-    @Mapping(target = "username", source = "user.name") // 중첩 객체의 필드를 평탄화하여 매핑
+    //@Mapping(target = "orderId", source = "id") // 이름이 다른 필드 매핑
+    //@Mapping(target = "username", source = "user.name") // 중첩 객체의 필드를 평탄화하여 매핑
     OrderResponse toResponse(Order order);
 
+    Order toEntity(OrderRequest request);
 }
